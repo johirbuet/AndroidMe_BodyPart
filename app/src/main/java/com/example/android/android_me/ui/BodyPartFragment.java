687fb0457e11d39318febf6124659a5ff5bb81ef
Johirul Islam <johirbuet@gmail.com>
2017-10-21 12:25:02 -0500

    Exercise of creating body part completed

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,31 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by mislam on 10/21/17.
+ */
+
+public class BodyPartFragment extends Fragment {
+    public BodyPartFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_body_part,container,false);
+        ImageView iv = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        iv.setImageResource(AndroidImageAssets.getHeads().get(0));
+        return rootView;
+    }
+}
